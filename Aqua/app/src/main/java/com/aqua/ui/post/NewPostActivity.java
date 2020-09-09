371e36f87e72a849c35f34b987b1fdb8378f302f
yuhan <ocsiua@gmail.com>
2020-09-09 21:43:14 +0800

    I am too slow, need update first

--- Aqua/app/src/main/java/com/aqua/ui/post/NewPostActivity.java
+++ Aqua/app/src/main/java/com/aqua/ui/post/NewPostActivity.java
@@ -1,9 +1,13 @@
 package com.aqua.ui.post;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.aqua.R;
+import com.aqua.data.Database;
 
 public class NewPostActivity extends AppCompatActivity {
 
@@ -11,6 +15,27 @@ public class NewPostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
+
+
+        //Get components
+        final EditText postEdit = findViewById(R.id.postEdit);
+        final Button sendButton = findViewById(R.id.sendButton);
+        final TextView textView = findViewById(R.id.textView2);
+
+        //Set sending listener
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String postText = postEdit.getText().toString(); //Get txt
+
+
+
+
+                postEdit.setText(null);
+            }
+        });
+
     }
 
 }
