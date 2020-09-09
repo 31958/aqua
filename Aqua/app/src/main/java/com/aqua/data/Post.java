e13c12b6e2264cd3448fcde0721b503977fb1475
yuhan <ocsiua@gmail.com>
2020-09-10 00:03:16 +0800

    Create class Post.java

---
+++ Aqua/app/src/main/java/com/aqua/data/Post.java
@@ -0,0 +1,9 @@
+package com.aqua.data;
+
+public class Post {
+    public String ID;
+    public String author_ID;
+    public String content;
+    public String dateTime;
+    public String location;
+}
